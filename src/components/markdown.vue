fb3ed97d0956e5671ebf3e0bbc46b9fd348b2051
ovenslove <1905997838@qq.com>
2017-03-25 12:09:47 +0800

    配置文件

---
+++ src/components/markdown.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="mdContainer">
+    {{msg}}
+</div>
+    
+</template>
+
+<script>
+    export default {
+        name: 'markdown',
+        data() {
+            return {
+                msg: 'sssdkgshdkg'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Markdown from '@/components/markdown'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'markdown',
+        component: Markdown
+    }]
 })
